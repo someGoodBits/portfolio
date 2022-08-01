9a11c3cb865e48a69a740af4ce9775d59d556d68
goodBits <ak.onhtrack@gmail.com>
2022-08-01 18:15:43 +0530

    Basic Setup

--- components/loader.js
+++ components/loader.js
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from "react";
+import gsap, { Power1 } from "gsap";
+import SpinnerFrame from "./Indicators/SpinnerFrame";
 
 const Loader = () => {
     const pathRef = useRef();
@@ -6,25 +8,33 @@ const Loader = () => {
     const [progress, setProgress] = useState(0);
 
     useEffect(() => {
-        setTotalLength(Math.ceil(pathRef.current.getTotalLength()));
+        let l = pathRef.current.getTotalLength();
+        setTotalLength(l);
+
+        gsap.to(pathRef.current,{
+            strokeDashoffset: (l+10) ,
+            duration : 2,
+            repeat : -1,
+            ease : Power1.easeOut
+        });
     }, []);
 
     return (
         <div className="fixed w-screen h-screen top-0 left-0 z-10 bg-gray-900">
             <div className="w-full h-full flex items-center justify-center">
-                <div></div>
+                {/* <SpinnerFrame /> */}
                 <svg width="65" height="65" viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                         ref={pathRef}
                         d="M47.9328 35.4115L59.2417 54.0925H41.6265L24.0113 24.9943L32.8189 10.4454L50.434 10.4454L24.0109 54.0925H6.39624L23.9727 25.0508"
-                        stroke="black"
                         strokeWidth="5"
                         strokeLinecap="square"
                         className="stroke-accent-500"
-                        // style={{
-                        //     strokeDasharray: `${totalLength} ${totalLength}`,
-                        //     strokeDashoffset: `${-totalLength + progress}`,
-                        // }}
+                        fill="none"
+                        style={{
+                            strokeDasharray: `${totalLength} ${2*totalLength}`,
+                            strokeDashoffset: `${-totalLength}`,
+                        }}
                     />
                 </svg>
             </div>
--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
     return (
         <Html>
             <Head>
-                <title>Anurag Kumar | Portfolio</title>
+                {/* <title>Anurag Kumar | Portfolio</title> */}
                 <meta name="description" content="My Portfolio" />
                 <link rel="icon" href="./favicon.png" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link
                     href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap"
                     rel="stylesheet"
